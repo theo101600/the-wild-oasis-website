4bdf5dba91a3e80eee26b29c06678955b57311ba
Theo <theo.lacanaria@gmail.com>
2025-08-13 23:21:34 +0800

    implemented loading for fetching remote state

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,18 @@
-import Navigation from "../components/Navigation";
+import Counter from "../components/Counter";
 
-export default function Page() {
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
   return (
     <div>
-      <h1>Cabins Page</h1>;
+      <h1>Cabins Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
     </div>
   );
 }
