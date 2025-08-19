0a080e6bd297c074c515a3313c4e4cb24ba0c846
Theo <theo.lacanaria@gmail.com>
2025-08-19 21:08:20 +0800

    organized files and activated tailwindcss

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
@@ -8,12 +10,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
         </header>
         <Navigation />
         <main>{children}</main>
+        <footer>Copyright by The Wild Oasis</footer>
       </body>
     </html>
   );
