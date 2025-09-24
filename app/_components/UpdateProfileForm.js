5a52f727c7f34a0298d71ddd9d5a463ce6e2f1bd
Theo <theo.lacanaria@gmail.com>
2025-09-24 21:03:48 +0800

    implement server actions for update guest data

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,19 +2,26 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import { updateGuest } from "../_lib/actions";
 
 const countryFlag = "/pt.jpg";
 const nationality = "/portugal";
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState();
+  const { id, fullName, email, nationalId, nationality, countryFlag } = guest;
 
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name="fullName"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -23,6 +30,8 @@ function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          defaultValue={email}
+          name="email"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -45,6 +54,7 @@ function UpdateProfileForm({ children }) {
         <label htmlFor="nationalID">National ID number</label>
         <input
           name="nationalID"
+          defaultValue={nationalId}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,6 +2,10 @@
 
 import { signIn, signOut } from "./auth";
 
+export async function updateGuest(formData) {
+  console.log(formData);
+}
+
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
